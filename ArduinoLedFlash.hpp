7a63874ccfeb7060fd75e2b28b24f2febd8c00a5
Greg Krsak <greg.krsak@gmail.com>
2018-12-31 10:29:50 -0800

    Fixed tabs

--- ArduinoLedFlash.hpp
+++ ArduinoLedFlash.hpp
@@ -7,12 +7,12 @@
 // https://github.com/gregkrsak/arduino_led_flash
 //
 // This is free and unencumbered software released into the public domain.
-// 
+//
 // Anyone is free to copy, modify, publish, use, compile, sell, or
 // distribute this software, either in source code form or as a compiled
 // binary, for any purpose, commercial or non-commercial, and by any
 // means.
-// 
+//
 // In jurisdictions that recognize copyright laws, the author or authors
 // of this software dedicate any and all copyright interest in the
 // software to the public domain. We make this dedication for the benefit
@@ -20,7 +20,7 @@
 // successors. We intend this dedication to be an overt act of
 // relinquishment in perpetuity of all present and future rights to this
 // software under copyright law.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -28,9 +28,9 @@
 // OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 // For more information, please refer to <http://unlicense.org/>
-// 
+//
 
 
 #pragma once
@@ -46,24 +46,24 @@
 //
 class ArduinoLedFlash : public MainProgram
 {
-public:
-	ArduinoLedFlash() { }
-	~ArduinoLedFlash() { }
-  
-  void setFlashIntervalTo(unsigned long newInterval);
-  unsigned long flashInterval();
-   
-  // From MainProgram
-  void enableDebugMode();
-  void changeStateTo(ProgramStateMachine newState);
-  ProgramStateMachine currentState();
-  void timeSlice();
-  
-protected:
-  unsigned long interval;
+  public:
+    ArduinoLedFlash() { }
+    ~ArduinoLedFlash() { }
 
-private:
-  unsigned long previousFlashTime;
+    void setFlashIntervalTo(unsigned long newInterval);
+    unsigned long flashInterval();
+
+    // From MainProgram
+    void enableDebugMode();
+    void changeStateTo(ProgramStateMachine newState);
+    ProgramStateMachine currentState();
+    void timeSlice();
+
+  protected:
+    unsigned long interval;
+
+  private:
+    unsigned long previousFlashTime;
 };
 
 
